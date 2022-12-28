4cb3584dec74c2db5ac286f62c5eed4705e8c3af
andres malaisse <malaisse_andres@sfr.fr>
2022-12-28 11:45:51 +0100

    first commit

--- screens/01LoginScreen/DiscoverBtn.js
+++ screens/01LoginScreen/DiscoverBtn.js
@@ -1,38 +1,35 @@
-import {
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-  } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-  export default function DiscoverBtn({ navigation }) {
+export default function DiscoverBtn({ navigation }) {
+  const handleDiscover = () => {};
 
-    const handleDiscover = () => {
+  return (
+    <TouchableOpacity
+      onPress={() => handleDiscover()}
+      style={styles.button}
+      activeOpacity={0.8}
+    >
+      <Text style={styles.textButton}>Discove</Text>
+    </TouchableOpacity>
+  );
+}
 
-    }
-    
-    return (
-        <TouchableOpacity onPress={() => handleDiscover()} style={styles.button} activeOpacity={0.8}>
-        <Text style={styles.textButton}>Discover</Text>
-        </TouchableOpacity>
-    )
-  }
-
-  const styles = StyleSheet.create({
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 8,
-        width: '40%',
-        height: '90%',
-        marginTop: 10,
-        backgroundColor: '#E74C3C',
-        borderRadius: 10,
-      },
-      textButton: {
-        color: '#ffffff',
-        height: 30,
-        fontWeight: '600',
-        fontSize: 16,
-        fontFamily: 'Poppins-Bold',
-      },
-  })
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 8,
+    width: "40%",
+    height: "90%",
+    marginTop: 10,
+    backgroundColor: "#E74C3C",
+    borderRadius: 10,
+  },
+  textButton: {
+    color: "#ffffff",
+    height: 30,
+    fontWeight: "600",
+    fontSize: 16,
+    fontFamily: "Poppins-Bold",
+  },
+});
